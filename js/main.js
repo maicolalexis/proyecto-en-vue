c7afdbc550940b6a37177c113252dea650d1ea9b
maicol alexis <maicolalexis222@gmail.com>
2022-05-19 19:02:15 -0500

    haciendo reactivo el input de busqueda

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 const API = "https://api.github.com/users/";
 const nombre = document.querySelector('#nombre')
+
+
 const app = Vue.createApp({
     data() {
         return {
-          message: 'Bienvenidos!'          
+          message: 'Bienvenidos!',
+          busqueda: null,
+          result: null         
         }
+        //la palabra function ya no es necesario para ya que se usa el method
+      },methods: {
+
+        async buscar(){
+          const response = await fetch (API + this.busqueda)
+          const data = await response.json()
+          console.log(data)
+          this.result = true;
       }
+      },
+      
 })//.mount('#app')//montamos esta informacion en el div app del html
-async function buscar(){
-    const response = await fetch (API + "maicolalexis")
-    const data = await response.json()
-    console.log(data)
-}
-buscar();
+
+

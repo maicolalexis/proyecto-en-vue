a0aa80cfbf4c5ca805a94b7f88473a80a25c00e1
maicol alexis <maicolalexis222@gmail.com>
2022-05-26 17:35:56 -0500

    se realizaron distintos cambios

--- js/main.js
+++ js/main.js
@@ -7,16 +7,33 @@ const app = Vue.createApp({
         return {
           message: 'Bienvenidos!',
           busqueda: null,
-          result: null         
+          result: null,
+          error: null        
         }
         //la palabra function ya no es necesario para ya que se usa el method
       },methods: {
+        
+          async buscar(){
 
-        async buscar(){
+            this.result = this.error = null
+          try {
+              
           const response = await fetch (API + this.busqueda)
+          if(!response.ok) throw new Error("Usuario no encontrado")
+          //
           const data = await response.json()
           console.log(data)
-          this.result = true;
+          
+          this.result = data;
+            
+          } catch (error) {
+           this.error = error
+          
+          //cuando termin el proceso limpia haciendo vacia la busqueda
+          }finally{
+            this.busqueda = null
+          }
+          
       }
       },
       

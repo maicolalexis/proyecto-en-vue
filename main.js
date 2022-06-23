4e585d0fb01cd77b3ef6eef583e45de88c948cb8
maicol alexis v <maicolalexis222@gmail.com>
2022-06-23 18:52:57 -0500

    se comenzo a organizar el proyecto

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-const API = "https://graph.facebook.com/v14.0/"
-const nombre = document.querySelector('#username');
+const API = "https://graph.facebook.com/"
 const app = Vue.createApp({
-    data() {
-        return {
-          message: 'Bienvenidos!',
-          busqueda: null,
-          result: null,
-          error: null,
-          favorito: new Map()
-        },
+  data() {
+    return {
+      busqueda: null,
+      result: null,
+      error: null,
+    }
+  }, methods: {
 
+    async buscar() {
+      try {
+        const response = await fetch(API + this.busqueda +'?fields=id,name,email,picture&access_token=EAAFaNshrhkgBAMJYcYrcbs4kDOOy6IcxQYmU0oIbBzbZB8DShij4KBO0vVjwlcZBrRSfYAbHPrKPQ1Qq446jVD41jdUarCZCtm70NXh3suTEUTWb1hF5F1frW1ZC6G2x7MV3PTyZCnFkPoZBaEe0requJGn1TDoG4HpN4NdS244QZDZD')
+        const data = await response.json()
+        console.log(data)
 
+        this.result = data;
+      } catch (error) {
+        this.error = error
 
-        created() {
-            
-        },
+        //cuando termin el proceso limpia haciendo vacia la busqueda
+      } finally {
+        this.busqueda = null
+      }
+
+    }
+  }
+})
